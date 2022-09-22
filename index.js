08e9cf3401e6d61ac819f726a087c4ecf7406f9c
Пользователь <shal7mov.v@gmail.com>
2022-09-22 17:40:12 +0500

    Updated

--- index.js
+++ index.js
@@ -11,10 +11,37 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function readLines(file){
+    return file.split("\r\n");
+}
+
+function findTODO(lines){
+    let TODOs = [];
+    lines.forEach(line => {
+        if (line.includes("// TODO "))
+           TODOs.push(line.split("// TODO ")[1]);
+    });
+    return TODOs;
+}
+
+function processTODOs(TODOs){
+    let processedTODOs = [];
+    TODOs.forEach(TODO => {
+        let splitedTODO = TODO.split("; ");
+        processedTODOs.push({name : splitedTODO[0], 
+                             date : splitedTODO[1], 
+                             comment : splitedTODO[2]});
+    });
+    return processedTODOs;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'test':
+            console.log(processTODOs(findTODO(readLines(getFiles()[0]))));
             break;
         default:
             console.log('wrong command');
