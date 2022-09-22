aedd4489255165b3d4fbe6bcb0a777a84db86ca9
nikitandro <nik.vish.93@mail.ru>
2022-09-22 18:14:07 +0500

    Points done: 1, 2, 3.

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
+const { getAllFilePathsWithExtension, readFile } = require("./fileSystem");
+const { readLine } = require("./console");
 
 const files = getFiles();
+const TODOs = files.map((file) => file.match(/\/\/ TODO .*/g)).flat(Infinity);
 
-console.log('Please, write your command!');
+console.log("Please, write your command!");
 readLine(processCommand);
 
 function getFiles() {
-    const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+    const filePaths = getAllFilePathsWithExtension(process.cwd(), "js");
+    return filePaths.map((path) => readFile(path));
 }
 
 function processCommand(command) {
     switch (command) {
-        case 'exit':
+        case "exit":
             process.exit(0);
+        case "show":
+            TODOs.forEach((element) => {
+                console.log(element);
+            });
+            break;
+        case "important":
+            TODOs.filter((item) => item.includes("!")).forEach((x) =>
+                console.log(x)
+            );
             break;
         default:
-            console.log('wrong command');
+            console.log("wrong command");
             break;
     }
 }
