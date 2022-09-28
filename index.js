81b242517ddaf199db6a4060bc0ad6c9c52efc82
AlexMokr <113508468+AlexMokr@users.noreply.github.com>
2022-09-28 20:41:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,61 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    command = command.split(' ');
+    switch (command[0]) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            console.log(showTodo());
+            break;
+        case 'important':
+            console.log (importantTodo())
+            break;
+        case 'user':
+            let user = command[1].charAt(0).toUpperCase()
+            userTodo(user)
+            break;
+        case 'sort':
+            switch (command[1]){
+                case 'importance':
+                    break;
+                case 'name':
+                    break;
+                case 'date':
+                    break;
+            }
+            break;
         default:
-            console.log('wrong command');
+            console.log("Def")
             break;
     }
+}
+function showTodo(){
+    let temp = getFiles().map(str => str.split("\r\n"));
+    temp = temp.flat(Infinity).filter(a => a.includes("// TODO") && !a.includes("temp ="));
+    temp = temp.map(a => a.slice(a.indexOf("/")))
+    return temp;
+}
+
+function importantTodo(){
+    return (showTodo().filter(a => a.includes("!")));
+}
+
+function userTodo(user){
+    console.log(showTodo().filter(a => a.includes(user)))
+}
+
+function sortImportance(){
+
+}
+
+function  sortUserName(){
+    
+}
+
+function sortData(){
+    
 }
 
 // TODO you can do it!
