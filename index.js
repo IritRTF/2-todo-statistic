57898e5646793c46cdb3f06337cf4dea757074d9
Alexey Bolotov <stealthfear@gmail.com>
2023-04-16 23:15:56 +0500

    Готова комманда show

--- index.js
+++ index.js
@@ -11,10 +11,28 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function findTODO(arr = [], expr = '// TODO'){
+    for (let i=0; i<arr.length; i++){
+        let cursorIndex = arr[i].indexOf(expr);
+        while (cursorIndex>0){
+            let endStringIndex = files[i].indexOf('\n', cursorIndex)
+            if (endStringIndex<0){
+                console.log(files[i].substring(cursorIndex));
+                continue;
+            }
+            console.log(files[i].substring(cursorIndex, endStringIndex))    
+            cursorIndex = arr[i].indexOf(expr, endStringIndex);
+        }
+    }
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;            
+        case 'show':
+            findTODO(files);
             break;
         default:
             console.log('wrong command');
