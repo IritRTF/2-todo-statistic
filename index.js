e8c97072446ce8528cd28d0995202677ef41bf65
Alexey Bolotov <stealthfear@gmail.com>
2023-04-17 13:23:25 +0500

    Готова выборка по пользователю

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
+const exp = require('constants');
 
 const files = getFiles();
 
@@ -11,26 +12,47 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
-function findTODO(arr = [], contains = '', expr = '// TODO') {
+function findTODO(arr = [], contains = '', expr = ['TODO']) {
     for (let i = 0; i < arr.length; i++) {
-        let cursorIndex = arr[i].indexOf(expr);
-        while (cursorIndex > 0) {
-            const endStringIndex = files[i].indexOf('\n', cursorIndex)
-            const substring = files[i].substring(cursorIndex, endStringIndex)
-            if (contains.length > 0) {
-                if (substring.includes(contains)) {
+        for (let exprNumb = 0; exprNumb < expr.length; exprNumb++) {
+            let cursorIndex = arr[i].indexOf(expr[exprNumb]);
+            while (cursorIndex > 0) {
+                const endStringIndex = files[i].indexOf('\n', cursorIndex)
+                const substring = files[i].substring(cursorIndex, endStringIndex)
+                if (contains.length > 0) {
+                    if (substring.includes(contains)) {
+                        console.log(substring)
+                    }
+                } else {
                     console.log(substring)
                 }
-            } else {
-                console.log(substring)
-            }
 
-            cursorIndex = arr[i].indexOf(expr, endStringIndex);
+                cursorIndex = arr[i].indexOf(expr, endStringIndex);
+            }
         }
     }
 }
 
-function processCommand(command) {
+function commandParse(command) {
+    const firstSpaceIndex = command.indexOf(' ');
+    const params = command.substring(firstSpaceIndex,).split(';')
+    return {
+        'command': firstSpaceIndex < 0 ? command : command.substring(0, firstSpaceIndex),
+        'user': params[0] ? params[0].trim() : '',
+        'date': params[1] ? params[1].trim() : '',
+        'comment': params[2] ? params[2].trim() : ''
+    }
+
+}
+
+function processCommand(str) {
+
+    let parse = commandParse(str)
+    const command = parse.command
+    const date = parse.date
+    const user = parse.user
+    const comment = parse.comment
+
     switch (command) {
         case 'exit':
             process.exit(0);
@@ -40,6 +62,13 @@ function processCommand(command) {
             break;
         case 'important':
             findTODO(files, contains = '!');
+            break;
+        case 'user':
+            const expressions = [
+                `TODO ${user.toUpperCase()}`,
+                `TODO ${user[0].toUpperCase()}${user.slice(1).toLowerCase()}`,
+                `TODO ${user.toLowerCase()}`]
+            findTODO(files, contains = '', expr = expressions);
             break;
         default:
             console.log('wrong command');
